bdf92ec44cc53da5f4d545f43f5226f223c0153c
SimoneDUrso <simone.durso98@gmail.com>
2024-07-04 15:30:33 +0200

    [UPD] Created variable = prompt and i invoked the function. First exercise completed.

--- palindroma/script.js
+++ palindroma/script.js
@@ -3,14 +3,16 @@ function palindroma(word) {
     let reverse = word.split("").reverse().join("")
 
     if (word == reverse) {
-        console.log(`${word} è palindroma.`)
+        console.log(`La parola ${word} è palindroma.`)
     }
 
     else {
-        console.log(`${word} non è palindroma.`)
+        console.log(`La parola ${word} non è palindroma.`)
     }
 }
 
+let promptWord = prompt("Inserisci qui la tua parola");
 
+palindroma(promptWord);
 
 
